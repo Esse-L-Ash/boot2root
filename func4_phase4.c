2d4667293a9d7992488f7319d293d0694a27e594
sle-huec <sle-huec@student.42.fr>
2026-02-12 00:56:38 +0100

    bash script to retrieve Laurie's password

---
+++ func4_phase4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int func4(int param_1)
+
+{
+    int iVar1;
+    int iVar2;
+
+    if (param_1 < 2)
+    {
+        iVar2 = 1;
+    }
+    else
+    {
+        iVar1 = func4(param_1 + -1);
+        iVar2 = func4(param_1 + -2);
+        iVar2 = iVar2 + iVar1;
+    }
+    return iVar2;
+}
+
+int main()
+{
+    printf("%d\n", func4(9));
+}
--- test_fun.bash
+++ test_fun.bash
@@ -9,20 +9,28 @@ FILES=$(grep -RE "getme|return"  ft_fun | cut -d':' -f1 | uniq)
 files_numerotation(){
     for FILE in $FILES
     do
-        #echo $FILE
         NUMBER_FILE=$(grep "//file" $FILE | cut -d e -f2)
-        #echo "file number is = $NUMBER_FILE"
         NEW=$(echo "${NUMBER_FILE}_${FILE}")
         echo $NEW
     done
 }
 
 get_pass() {
-    # s'il croise un nombre il le prend en compte pas seulement le char
-    for c in $(cat $(files_numerotation | sort -h | cut -d _ -f 2-) | grep -E "return" | sed "s/.*'\(\w\)'.*/\1/" )
+    # echo "result of the grep on //file:"
+    # files_numerotation
+    # echo "Now, let's sort the files"
+    # files_numerotation | sort -h
+    # echo "We can now, display the str in the right order thanks to sed who isolate the char and echo -n"
+
+    # /!\ THE LINES ABOSE TEMPERS with the sha256 sum ! /!\
+
+    for c in $(cat $(files_numerotation | sort -h | cut -d _ -f 2-) | grep -E "return" | sed "s/.*'\(\w\)'.*/\1/")
     do
         echo -n "$c"
     done
 }
 
+get_pass
+echo ""
+echo "let's use sha256sum on this string"
 get_pass | sha256sum
